131a85a49928028ed3ab6f120fea8d86a02ad773
novitasalsabila <novnovv10@gmail.com>
2024-11-05 21:05:29 +0700

    Selesai

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const todoRoutes = require('./routes/todo.js');
+const todoRoutes = require('./routes/tododb.js');
 const app = express();
-const port = 3000;
+require('dotenv').config();
+const port = process.env.PORT;
 
 app.use(express.json());
 
